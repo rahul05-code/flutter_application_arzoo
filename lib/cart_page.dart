d9065f268f082c5ce4260d26d1c18e1c61106127
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-17 08:50:35 +0530

    scrool

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -6,43 +6,34 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:Text('Cart Page'),
+      appBar: AppBar(title: Text('Cart Page')),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) => cartItem(),
       ),
-      body:Column(
-        children: [
-          cartItem(),
-          cartItem(),
-          cartItem(),
-        ],
-      )
     );
-    
   }
-  Widget cartItem(){
+
+  Widget cartItem() {
     return Card(
-        child:SizedBox(
-          height:120,
-          child: Row(
-            children: [
-              Container(
-                height:100,
-                width:100,
-                color:Colors.deepPurple,
+      child: SizedBox(
+        height: 120,
+        child: Row(
+          children: [
+            Container(height: 100, width: 100, color: Colors.deepPurple),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Prodcut Name', style: TextStyle(fontSize: 30)),
+                  Text('Description', style: TextStyle(fontSize: 15)),
+                ],
               ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Prodcut Name',style:TextStyle(fontSize: 30),),
-                    Text('Description',style:TextStyle(fontSize: 15),),
-                  ],
-                ),
-              ),
-              Text("500",style: TextStyle(fontSize: 50),)
-            ],
-          ),
-        )
-      );
+            ),
+            Text("500", style: TextStyle(fontSize: 50)),
+          ],
+        ),
+      ),
+    );
   }
 }
